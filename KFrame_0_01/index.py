c828c043f446389da6352bde7dfad521a311da1b
Khasm08 <klbs825@gmail.com>
2015-12-06 20:43:52 +0000

    WSGI app test.

--- KFrame_0_01/index.py
+++ KFrame_0_01/index.py
@@ -1,7 +1,29 @@
-def application(environ, start_response):
-  #Simple application object
-  status = '200 OK'
-  response_headers[('Contect-type', 'text/plain')]
-  start_response(status, response_headers)
-  return ['Hello, World!\n']
+HELLO_WORLD = """<html>
+<head>
+    <title>Python Anywhere hosted web application</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>
+    This is the default welcome page for a
+    <a href="https://www.pythonanywhere.com/">PythonAnywhere</a>
+    hosted web application.
+</p>
+<p>
+    Find out more about how to configure your own web application
+    by visiting the <a href="https://www.pythonanywhere.com/web_app_setup/">web app setup</a> page
+</p>
+</body>
+</html>"""
 
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO') == '/':
+        status = '200 OK'
+        content = HELLO_WORLD
+    else:
+        status = '404 NOT FOUND'
+        content = 'Page not found.'
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+    start_response(status, response_headers)
+    yield content.encode('utf8')
